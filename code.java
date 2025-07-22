c6aba2a42e45a5c2e4e449f8c5b46ec9d95948ba
coco0557 <jaipaulsooriya05@gmail.com>
2025-07-22 13:48:59 +0100

    code.java

---
+++ code.java
@@ -0,0 +1,44 @@
+Here is the code so far. i have only done the meat and seafood section.
+array list was used
+
+  import java.util.ArrayList;
+
+public class MeatAndSeafood {
+    
+  private ArrayList<String> items;
+  
+  //constructor
+  public MeatAndSeafood(ArrayList<String> items) {
+      this.items = items;
+  }
+  
+  
+  public void addItem(String item){
+      items.add(item);
+  }
+  public void displayItems() {
+      System.out.println("===Meat and Seafood items===");
+for (String item : items) {
+    System.out.println("- " + item);
+}
+  }
+  public ArrayList<String> getItems() { 
+      return items;
+  }
+  public static void main(String[] args) {
+         ArrayList<String> meatList = new ArrayList<>();
+        meatList.add("Chicken breast (TDH)");
+        meatList.add("Cooked chicken (C Vale)");
+        meatList.add("Spare rib meat (Ho Wan)");
+        meatList.add("Pork neck (13.6kg)");
+        meatList.add("Duck 2.6 county");
+        meatList.add("Cooked duck breast");
+        meatList.add("King prawn (31/40)");
+        meatList.add("Squid tubes");
+        meatList.add("Crabmeat");
+        meatList.add("Shrimp 100/200");
+        meatList.add("Scallop");
+        MeatAndSeafood restock = new MeatAndSeafood(meatList);
+        restock.displayItems();
+  }
+}
