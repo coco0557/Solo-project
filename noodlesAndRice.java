db2efbcb6d835bb4d33c289ebda28492585a0816
J <jaipaulsooriya05@gmail.com>
2025-07-29 17:00:49 +0100

    noodlesAndRice.java

---
+++ noodlesAndRice.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+public class noodlesAndRice {
+    private ArrayList<String> items;
+    
+    // constructor
+    public noodlesAndRice(ArrayList<String> items) {
+        this.items = items;
+    }
+    
+    
+    public void addItem(String item){ 
+        items.add(item);
+    }
+    public void displayItems() {
+        System.out.println("===Noodles And Rice===");
+ for (String item : items){
+     System.out.println("- " + item);
+ }       
+    }
+    public ArrayList<String> getItems(){
+        return items;
+    } 
+    
+    public static void main(String[] args) {
+        ArrayList<String>NoodleRiceList = new ArrayList<>();
+        NoodleRiceList.add("Rice");
+        NoodleRiceList.add("Noodle");
+        NoodleRiceList.add("Udon");
+        NoodleRiceList.add("Rice Sticks");
+        NoodleRiceList.add("Vietnamese rice paper");
+        NoodleRiceList.add("Rice stick ban pho");
+        noodlesAndRice restock = new noodlesAndRice(NoodleRiceList);
+        restock.displayItems();
+    }
+    
+}
