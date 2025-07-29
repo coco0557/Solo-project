800627bfe1a8fc72df09b2e914b17cc86d7f1b67
J <jaipaulsooriya05@gmail.com>
2025-07-29 17:29:48 +0100

    CannedAndJarred.java

---
+++ CannedAndJarred.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class CannedAndJarred {
+    
+    private ArrayList<String> items;
+
+    public CannedAndJarred(ArrayList<String> items) {
+        this.items = items;
+    }
+    
+    public void addItem(String item){
+        items.add(item);
+    }
+    public void displayItems() {
+        System.out.println("===Canned and Jarred Goods===");
+for (String item : items) {
+    System.out.println("- " + item);
+}
+    }
+    public ArrayList<String> getItems() {
+        return items;
+    }
+    public static void main(String[] args) {
+        ArrayList<String> goodsList = new ArrayList<>();
+        goodsList.add("Chinese Shredded Radish");
+        goodsList.add("Cream Style Corn");
+        goodsList.add("Lychees");
+        goodsList.add("Pineapple Pieces(L)");
+        goodsList.add("Water Chestnut");
+        goodsList.add("1/2 Straw Mushrooms");
+        goodsList.add("Poku Mushrooms");
+        goodsList.add("Ground Fried Fish");
+        CannedAndJarred restock = new CannedAndJarred(goodsList);
+        restock.displayItems();
+    }
+    
+}
