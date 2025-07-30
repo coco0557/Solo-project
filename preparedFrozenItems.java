22fd911dcac49da8a609a72eed265e3d496fd0b2
J <jaipaulsooriya05@gmail.com>
2025-07-30 08:21:26 +0100

    Update preparedFrozenItems.java

--- preparedFrozenItems.java
+++ preparedFrozenItems.java
@@ -35,3 +35,32 @@ for (String item : items) {
     }
     
 }
+
+// new update for the code - i have added the StockCategory and InventoryManager:
+// here is the updated code using the both of them
+
+import java.util.ArrayList;
+public class preparedFrozenItems extends StockCategory {
+    
+    public preparedFrozenItems(ArrayList<String> items) {
+        super(items); 
+    }
+  
+    public static void main(String[] args) {
+        ArrayList<String>FrozenList = new ArrayList<>();
+        FrozenList.add("Pork Gyoza");
+        FrozenList.add("Veg Gyoza");
+        FrozenList.add("Chicken Gyoza");
+        FrozenList.add("Veg spring Roll");
+        FrozenList.add("Wonton Pastry");
+        FrozenList.add("Breaded Torpedo KP");
+        FrozenList.add("Duck Skin (pancake)");
+        preparedFrozenItems restock = new preparedFrozenItems(FrozenList);
+        
+        InventoryManager manager = new InventoryManager();
+        manager.addCategory(restock);
+        manager.displayAllStock();
+        
+    }
+    
+}
