ac466b97b078c3687f195f2664282fac1dc04edc
J <jaipaulsooriya05@gmail.com>
2025-07-30 08:23:31 +0100

    Update CannedAndJarred.java

--- CannedAndJarred.java
+++ CannedAndJarred.java
@@ -34,3 +34,33 @@ for (String item : items) {
     }
     
 }
+
+
+// new update for the code - i have added the StockCategory and InventoryManager:
+// here is the updated code using the both of them
+
+import java.util.ArrayList;
+public class CannedAndJarred extends StockCategory {
+    
+    public CannedAndJarred(ArrayList<String> items) {
+        super(items); 
+    }
+  
+    public static void main(String[] args) {
+        ArrayList<String> goodsList = new ArrayList<>();
+        goodsList.add("Chinese Shredded Radish");
+        goodsList.add("Cream Style Corn");
+        goodsList.add("Lychees");
+        goodsList.add("Pineapple Pieces(L)");
+        goodsList.add("Water Chestnut");
+        goodsList.add("1/2 Straw Mushrooms");
+        goodsList.add("Poku Mushrooms");
+        goodsList.add("Ground Fried Fish");
+        CannedAndJarred restock = new CannedAndJarred(goodsList);
+        
+        InventoryManager manager = new InventoryManager();
+        manager.addCategory(restock);
+        manager.displayAllStock();
+    }
+    
+}
