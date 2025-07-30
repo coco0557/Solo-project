9fffb188f9179ac6deb9115c05c0a22d5d5a2922
J <jaipaulsooriya05@gmail.com>
2025-07-30 09:27:07 +0100

    Update InventoryManager.java

--- InventoryManager.java
+++ InventoryManager.java
@@ -10,6 +10,18 @@ public class InventoryManager {
     public void addCategory(StockCategory category) {
         categories.add(category);
     }
+    public void searchItem(String itemName) {
+    for (StockCategory category : categories) {
+        for (Item item : category.getItems()) {
+            if (item.getName().equalsIgnoreCase(itemName)) {
+                String categoryName = category.getClass().getSimpleName();
+                System.out.println("Found: " + item.getName() + " (Qty: " + item.getQuantity() + ") in " + categoryName);
+                return;
+            }
+        }
+    }
+    System.out.println("Item not found: " + itemName);
+}
 
     public void displayAllStock() {
         for (StockCategory category : categories) {
