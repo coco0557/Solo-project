24fb516fcc6f9c2c020be1a2ea1e1cfa6a431889
J <jaipaulsooriya05@gmail.com>
2025-07-30 08:38:37 +0100

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,25 @@
+public class Item {
+    private String name;
+    private int quantity;
+
+    public Item(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String toString() {
+        return name + " (Qty: " + quantity + ")";
+    }
+}
